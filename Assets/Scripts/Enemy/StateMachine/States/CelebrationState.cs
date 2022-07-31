fc8289b6286c21c80db3dbd34f45180c34493168
Roma <rlepesh@mail.ru>
2022-08-01 02:16:49 +0300

    Add sprites, stateMachine, animation, weapons, background

---
+++ Assets/Scripts/Enemy/StateMachine/States/CelebrationState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CelebrationState : State
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _animator.Play("Celebration");
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Transform _shootPoint;
+
+    private Weapon _currentWeapon;
+    private int _currentHealth;
+    private Animator _animator;
+
+    public int Money { get; private set; }
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _currentWeapon = _weapons[0];
+        _currentHealth = _health;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentWeapon.Shoot(_shootPoint);
+        }
+    }
+
+    private void OnEnemyDied(int reward)
+    {
+        Money += reward;
+    }
+
+    public void ApplyDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if(_currentHealth<=0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
